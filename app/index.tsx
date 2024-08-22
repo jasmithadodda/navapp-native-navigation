ea70770ac8668517a92239f4dd3897cea53e1a8c
jasmithadodda <jasmithadodda@gmail.com>
2024-08-22 13:04:50 +0530

    initial commit with home page

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+const router = useRouter();
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => router.push('/home')}
+      /><br/>
+      <Button
+        title="About"
+        onPress={() => router.push('/about')}
+      />
+
+    </View>
+  );
+}
