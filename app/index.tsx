3e594a1189f043d0b9ebac7cb743d9c0df928022
jasmithadodda <jasmithadodda@gmail.com>
2024-08-23 16:00:44 +0530

    Customizing tab appearance

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Homescreen from "./mytabs/homescreen";
+import SettingsScreen from "./mytabs/SettingsScreen";
+import { Ionicons } from "@expo/vector-icons";
 
+const Tab = createBottomTabNavigator();
 export default function Index() {
-    return (
-        <View>
-            <Text>Hello tabs</Text>
-        </View>
-  )
+  return (
+    <NavigationContainer independent={true}>
+       <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={Homescreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
