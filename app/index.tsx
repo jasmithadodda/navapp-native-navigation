325319a3a88f6c7cf61a9987efb3beeff428d50a
jasmithadodda <jasmithadodda@gmail.com>
2024-08-23 12:56:14 +0530

    Replacing the title with a custom component

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { Button, Text, View, StyleSheet, Platform } from "react-native";
+import { Button, Text, View, StyleSheet, Platform, Pressable, } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./home";
 import Details from "./screens/details";
 import CreatePostScreen from "./screens/createPost";
 import profile from "./screens/profile";
+import Logotitle from "./screens/logotitle";
 
 //const router = useRouter();
 const Stack = createNativeStackNavigator();
@@ -28,9 +29,27 @@ export default function Index() {
     <Stack.Screen
           name="Home"
           component={Home}
-          options={{ title: "Dashboard" }}
+          options={{
+            title: "Dashboard",
+            headerStyle: {
+              backgroundColor: "#6a51ae",
+            },
+            headerTintColor: "#abcdef",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerLeft: () => (
+              <Pressable
+                onPress={() => {
+                  alert("menu pressed");
+                }}
+              >
+                <Text>Menu</Text>
+              </Pressable>
+            ),
+          }}
         />
-        <Stack.Screen
+    <Stack.Screen
           name="Details"
           component={Details}
           initialParams={{ itemId: 42 }}
@@ -41,13 +60,13 @@ export default function Index() {
           options={{ title: "write mail" }}
           />
   
-          <Stack.Screen
+    <Stack.Screen
             name="Profile"
             component={profile}
           options={({ route }) => ({ title: route.params.name })}
         />
      
-      </Stack.Navigator>
+    </Stack.Navigator>
     </NavigationContainer>
   );
   
---
+++ app/screens/logotitle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image } from "react-native";
+
+function Logotitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+    />
+  );
+}
+
+export default Logotitle;
