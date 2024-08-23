3406b6c590e64fc5ddc7e5207efab55460f24e7f
jasmithadodda <jasmithadodda@gmail.com>
2024-08-23 11:06:31 +0530

    navigate,goback,poptotop

--- app/home.tsx
+++ app/home.tsx
@@ -26,6 +26,7 @@ export default function Home({navigation}) {
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       />
+      
     </View>
   );
 }
--- app/screens/details.tsx
+++ app/screens/details.tsx
@@ -9,6 +9,12 @@ function Details({navigation}) {
         title="Go to Details... again"
         onPress={() => navigation.push("Details")}
       />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
